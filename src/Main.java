9bc6c717dec1061d02d654fc537461a496af1b1f
rkyadav1989 <rakesh.bmas056@gmail.com>
2021-12-15 11:06:23 +0100

    datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is my first Java Program");
+        int studentAge = 15;
+        boolean studentHasPerfectAttendence = true;
+        char studentFirstInitials = 'K';
+        double studentGPA = 3.45;
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+        System.out.println(studentFirstName + " " +
+                studentLastName + " has a GPA of " + studentGPA);
+        char studentLastInitials = studentLastName.charAt(0);
+
+        System.out.println("What do you want to update it to?");
+//read user input
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName +
+                " now has a GPA of " + studentGPA);
     }
 }
