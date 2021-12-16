dd1a4740108ea815426c69cf5ae7a15f1c7a204a
rkyadav1989 <rakesh.bmas056@gmail.com>
2021-12-16 07:28:02 +0100

    builtInFunction01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my first Java Program");
+        //Math is pre-defined in Java library and pow is function defined under this, which accepts two double values and retrun exponantial
+        double result = Math.pow(10,2);
+        System.out.println(result);
     }
 }
