d550cc1e4d5427279f821945405b5303e11df0c1
rkyadav1989 <rakesh.bmas056@gmail.com>
2021-12-16 06:55:36 +0100

    function01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time....");
+        System.out.println("Please type a random word to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's Developer tea time!!!");
+    }
     public static void main(String[] args) {
-        System.out.println("This is my first Java Program");
+        //call the function
+        announceDeveloperTeaTime();
+
+        System.out.println("we will call our function again");
+
+        announceDeveloperTeaTime();
     }
 }
