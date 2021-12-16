26c625e5cee7f31712aeec2905ae5d525523956e
rkyadav1989 <rakesh.bmas056@gmail.com>
2021-12-16 07:17:42 +0100

    parameterized functions with return type

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    //define function to calculate the cost of meal
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = tip + tax + listedMealPrice;
+        System.out.println("Your total meal price is EUR " + result);
+    }
+
+    //define similar function to calculate the cost of meal and RETURN the result
+    public static double calculateMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = tip + tax + listedMealPrice;
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println("This is my first Java Program");
+        System.out.println("");
+        calculateTotalMealPrice(100,.2,.1);
+        calculateTotalMealPrice(100,.3,.1);
+
+        double mealPrice=calculateMealPrice(100,.2,.1);
+        System.out.println("Your returned meal price is EUR " + mealPrice);
     }
 }
