8af4ccbcd6aaf64a06ac6525c6c696f55fc2176e
rkyadav1989 <rakesh.bmas056@gmail.com>
2022-01-11 23:10:04 +0100

    DataStrucre

--- src/WorkingWithQueue.java
+++ src/WorkingWithQueue.java
@@ -4,14 +4,28 @@ import java.util.Queue;
 public class WorkingWithQueue {
     public static void main(String[] args) {
 
-        Queue<Person> supermarket = new LinkedList<>();
-        supermarket.add(new Person("Alex", 21));
+        //Queue<Person> supermarket = new LinkedList<>();
+        //supermarket.add(new Person("Alex", 21));
+        Queue<String> supermarket = new LinkedList<>();
+        supermarket.add("Alex");
+        supermarket.add("Garry");
+        supermarket.add("Sonu");
 
+        System.out.println(supermarket);
+        System.out.println(supermarket.peek()); //start of queue
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.poll()); //removes from queue
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
 
 
     }
 
-    //static Person(String name, int age) {}
-    record Person(String name, int age) {}
+    /**
+     * We wanted to expirement record class of java 14 but it has not compatible with intellij 2019; so we will do experiment with String
+     */
+
+    //static record Person(String name, int age) {}
+
 
 }
