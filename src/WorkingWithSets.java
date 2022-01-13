9d783cd2bc40a97d5f2e1a9f88bdaaa5c83e5efe
rkyadav1989 <rakesh.bmas056@gmail.com>
2022-01-13 11:58:56 +0100

    Sets

--- src/WorkingWithSets.java
+++ src/WorkingWithSets.java
@@ -1,8 +1,18 @@
+import java.util.HashSet;
 import java.util.Set;
 
 public class WorkingWithSets {
     public static void main(String[] args) {
-        Set<String>
-
+        Set<String> colors = new HashSet<>();
+        colors.add("blue");
+        colors.add("blue"); //here we try to add blue color twice but it will be added only once because set can have unique elements
+        colors.add("red");
+        colors.add("green");
+        System.out.println(colors.size());
+        colors.forEach(System.out::println);
+        colors.remove("green");
+        System.out.println("----------");
+        System.out.println(colors.size());
+        colors.forEach(System.out::println);
     }
 }
